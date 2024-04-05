4436cbb123957f5ba5d560aa2a65df6506331903
Kevin Camargo <37909922+kjccc1993@users.noreply.github.com>
2024-04-05 16:30:46 -0400

    Firebase production configs

--- functions/config/index.js
+++ functions/config/index.js
@@ -1,12 +1,12 @@
 const firebaseConfigWeb = {
-  apiKey: 'AIzaSyAN_GhnlypjusK6oEnn10US9KKyjR16QWc',
-  authDomain: 'carsales-dev-6350a.firebaseapp.com',
-  projectId: 'carsales-dev-6350a',
-  storageBucket: 'carsales-dev-6350a.appspot.com',
-  messagingSenderId: '943837398663',
-  appId: '1:943837398663:web:032f41859605c47b2b5a22',
-  measurementId: 'G-6THJCX68VL'
-};
+  apiKey: 'AIzaSyDADpm6XUMeyOZJXhSoZSEJaKLo0TzOnwA',
+  authDomain: 'vroomit-prod.firebaseapp.com',
+  projectId: 'vroomit-prod',
+  storageBucket: 'vroomit-prod.appspot.com',
+  messagingSenderId: '320839684152',
+  appId: '1:320839684152:web:db9b556f581fcd534c96cd',
+  measurementId: 'G-BNZGQHPSZ0'
+}
 
 const openAI = {
   apiKey: process.env.OPEN_AI_API_KEY,
