27116d0a038a162c9d473b6e4875d1bee3d06241
Kevin Camargo <37909922+kjccc1993@users.noreply.github.com>
2024-04-20 20:19:48 -0400

    fix variable

--- functions/api-rest-server/controllers/mailjet/mailjetController.js
+++ functions/api-rest-server/controllers/mailjet/mailjetController.js
@@ -4,47 +4,47 @@ const httpService = require('../../services/http.service')
 // Initialize Firebase
 
 const sendEmail = async (req, res) => {
-    logger.log(req.body)
-    logger.log(req.headers)
-  
-    const { To, TemplateId, Subject, variables } = req.body
-    try {
-      const postData = {
-        Messages: [
-          {
-            From: {
-              Email: 'no-responder@vroomit.io',
-              Name: 'Vroomit'
-            },
-            To: [
-              {
-                Email: To
-              }
-            ],
-            TemplateID: TemplateId,
-            TemplateLanguage: true,
-            Subject: Subject,
-            Variables: variables
-          }
-        ]
-      }
-  
-      const response = await httpService.post({
-        url: `https://api.mailjet.com/v3.1/send`,
-        postData: postData,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Basic YTNhZjNhYzVmY2YzNjQ3ODIwZTMxYzI1N2U5N2M2MjA6ZjZlNDZmY2JkZmI5OWFlNzdlN2NhOTZiZjFmZTNkODY=`
+  logger.log(req.body)
+  logger.log(req.headers)
+
+  const { To, TemplateId, Subject, Variables } = req.body
+  try {
+    const postData = {
+      Messages: [
+        {
+          From: {
+            Email: 'no-responder@vroomit.io',
+            Name: 'Vroomit'
+          },
+          To: [
+            {
+              Email: To
+            }
+          ],
+          TemplateID: TemplateId,
+          TemplateLanguage: true,
+          Subject,
+          Variables
         }
-      })
-  
-      res.status(200).send(response.data)
-    } catch (error) {
-        logger.error(error)
-        res.status(400).send('Error al enviar el correo')
+      ]
     }
+
+    const response = await httpService.post({
+      url: `https://api.mailjet.com/v3.1/send`,
+      postData: postData,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic YTNhZjNhYzVmY2YzNjQ3ODIwZTMxYzI1N2U5N2M2MjA6ZjZlNDZmY2JkZmI5OWFlNzdlN2NhOTZiZjFmZTNkODY=`
+      }
+    })
+
+    res.status(200).send(response.data)
+  } catch (error) {
+    logger.error(error)
+    res.status(400).send('Error al enviar el correo')
   }
-  
-  module.exports = {
-    sendEmail
-  }
+}
+
+module.exports = {
+  sendEmail
+}
